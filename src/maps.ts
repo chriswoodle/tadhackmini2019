8317c3c7c8c1a7e1ff2b88b3afcb8927109ee571
Chris Woodle <chris@droplit.io>
2019-03-16 17:12:10 -0400

    fixed google maps typings

--- src/maps.ts
+++ src/maps.ts
@@ -1,14 +1,14 @@
-import { createClient, GoogleMapsClient } from '@google/maps';
+import * as maps from '@google/maps';
 
 const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
 if (!GOOGLE_MAPS_API_KEY) throw new Error('Missing process.env.GOOGLE_MAPS_API_KEY!');
 
-const client = createClient({ key: GOOGLE_MAPS_API_KEY });
+const client = maps.createClient({ key: GOOGLE_MAPS_API_KEY });
 
-export function geocodeAddress(address: string): Promise<google.maps.GeocoderResult[]> {
-    const request: google.maps.GeocoderRequest = { address };
+export function geocodeAddress(address: string) {
+    const request: maps.GeocodingRequest = { address };
 
-    return new Promise<google.maps.GeocoderResult[]>((resolve, reject) => {
+    return new Promise<maps.GeocodingResult[]>((resolve, reject) => {
         client.geocode(request, (error, response) => {
             if (error) {
                 reject(error);
