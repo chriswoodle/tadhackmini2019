445e483317d64edc1203e42052f354647daea447
Hunter <hunter.r.henry@gmail.com>
2019-03-17 11:51:03 -0400

    Translations now available!

--- scripts/translate.py
+++ scripts/translate.py
@@ -1,15 +1,17 @@
 import requests
 import sys
 import json 
-import os 
-import sklearn 
-import pandas as pd 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_extraction.text import TfidfVectorizer
 
-message = str(sys.argv[1])
-language = str(sys.argv[2])
-api_key = str(sys.argv[3])
+raw_text = str(sys.argv[1])
+api_key = str(sys.argv[2])
+
+def translate(raw_text):
+    break_pos = raw_text.find(':')
+    language = raw_text[:break_pos]
+    language = language.replace(' ', '' )
+    message = raw_text[break_pos + 1:]
+    return language, message
+
 
 def get_translation(language, message, translate_api):
     lang_code = get_language(language.lower())
@@ -20,16 +22,12 @@ def get_translation(language, message, translate_api):
         resp = r.json()
         new_text = dict(resp['data']['translations'][0])
 
-        # ':' is used to signify a translation, if missing, predicts the question type
-        if new_text['translatedText'].count(':') == 0:
-            new_text['type'] = classifier(new_text['translatedText'])
-        else:
-            new_text['type'] = 4
-
     else:
         new_text = dict(r.json())
     new_text = json.dumps(new_text)
     return new_text
+
+
 
 def get_language( language_name):
     # Gets the abbreviation for the language requested
@@ -144,36 +142,5 @@ def get_language( language_name):
     return language_name
 
 
-
-
-
-def classifier(message):
-    # cur_directory = 'C:/Users/h3576/OneDrive - University of Florida/Projects/tadhackmini2019/scripts'
-    # os.chdir(cur_directory)
-    df_data = pd.read_csv('classifier_data.csv')
-    df_data = df_data.loc[df_data['Target'] != 4]
-    vectorizer = TfidfVectorizer()
-    vectorizer.fit_transform(df_data['Text'])
-    temp = vectorizer.transform(df_data['Text'])
-
-
-
-    # Model Builder
-    new_question = 'where is Mcdonalds?'
-    clf = RandomForestClassifier()
-    clf.fit(temp, df_data['Target'])
-    input_question = vectorizer.transform([message])
-    return str(clf.predict(input_question)[0])
-
-# Cases
-# 1 FindNearest
-# 2 Navigate
-# 3 MedicineLookup
-# 4 Translate
-# 5 WhatDis
-# 6 News
-# 7 Help
-# 8 Hello
-# 9 Stop
-
+language, message = translate(raw_text)
 print(get_translation(language, message, api_key))
