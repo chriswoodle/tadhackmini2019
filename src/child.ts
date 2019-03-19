305d180694f2941486252d81c97394967995aa80
Chris Woodle <chris@droplit.io>
2019-03-19 12:57:41 -0400

    comment

--- src/child.ts
+++ src/child.ts
@@ -1,5 +1,6 @@
 import { exec as Exec } from 'child_process';
 
+// Spawn a child process to the CLI and parse stdout as json
 export function exec<T>(cwd: string, ...params: string[]) {
     return new Promise<T>((resolve, reject) => {
         Exec(params.join(' '), { cwd }, (err, stdout, stderr) => {
